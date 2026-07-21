d027e727faf98ad0b20ca3943511ca4db01447d5
radwaw1 <166555572+radwaw1@users.noreply.github.com>
2026-07-21 17:46:06 +0100

    Update newbwsscript.lua

--- newbwsscript.lua
+++ newbwsscript.lua
@@ -8,7 +8,7 @@ local Window = Rayfield:CreateWindow({
    ShowText = "script", -- for mobile users to unhide Rayfield, change if you'd like
    Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-   ToggleUIKeybind = "RightShift", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
 
    DisableRayfieldPrompts = false,
    DisableBuildWarnings = false, -- Prevents Rayfield from emitting warnings when the script has a version mismatch with the interface.
